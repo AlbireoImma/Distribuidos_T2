7556078ac71e194f50122d77933f743323c59b9c
Francisco Abarca <francisco.abarcamo@sansano.usm.cl>
2019-12-31 19:20:20 -0300

    [FIN] To Testing

--- Servidor.py
+++ Servidor.py
@@ -4,6 +4,12 @@ import grpc
 import proto.chat_pb2 as Chat
 import proto.chat_pb2_grpc as RPC
 import time
+from datetime import datetime
+
+def ts():
+        tiempo = datetime.now()
+        tiempo = tiempo.time().strftime("%H:%M:%S")
+        return tiempo
 
 class ServidorChat(RPC.ServidorChatServicer):
     
@@ -21,9 +27,44 @@ class ServidorChat(RPC.ServidorChatServicer):
                 yield men
     
     def EnviarMensaje(self, request: Chat.Mensaje, context):
-        print("[{}] {} {}".format(request.id,request.ts,request.mensaje))
+        print("mensaje de {} para {}".format(request.sender,request.destino))
+        archivo = open("log.txt","a+")
+        log = "[{}] ({}) {} --> {} {}\n".format(request.ts,request.id,request.sender,request.destino,request.mensaje)
+        archivo.write(log)
         self.chats.append(request)
         return Chat.Nulo()
+    
+    def Registrar(self, request: Chat.Registro, context):
+        self.clientes.append(request.user)
+        archivo = open("log.txt","a+")
+        time = ts()
+        time = "[" + time + "]"
+        archivo.write(time + " Usuario " + request.user + " conectado\n")
+        print("Usuario [{}] registrado".format(request.user))
+        return Chat.Nulo()
+
+    def ObtenerLista(self, request, context):
+        lista = Chat.Listado()
+        aux = ""
+        for user in self.clientes:
+            user += " "
+            aux += user
+        lista.lista = aux
+        return lista
+
+    def Desconectar(self, request: Chat.Registro, context):
+        user = request.user
+        archivo = open("log.txt","a+")
+        self.clientes.append(request.user)
+        time = ts()
+        time = "[" + time + "]"
+        archivo.write(time + " Usuario " + request.user + " desconectado\n")
+        print("Usuario [{}] Desconectado".format(user))
+        if user in self.clientes:
+            self.clientes.remove(user)
+        return Chat.Nulo()
+    
+    
 
 if __name__ == "__main__":
     puerto = 12000 # Puerto del servidor
