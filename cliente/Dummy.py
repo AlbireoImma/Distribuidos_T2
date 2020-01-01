a701beda9c8ab8176c0ab3e2a9af28e25c049842
Francisco Abarca <francisco.abarcamo@sansano.usm.cl>
2019-12-31 21:45:47 -0300

    Dockerizacion

---
+++ cliente/Dummy.py
@@ -0,0 +1,8 @@
+import time
+
+print("Arquitectura levantada")
+print("Para ejecutar una terminal de cliente ejecutar:")
+print("$ docker exec -it Cliente python ./Cliente.py")
+
+while True:
+    time.sleep(100)
